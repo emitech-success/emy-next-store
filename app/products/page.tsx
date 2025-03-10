db37a8b0fb90af895df4637c26471d3ebc875984
emitech-success <odezuluaugustine@gmail.com>
2025-03-10 11:24:12 +0100

    changed the type to any because next 15.2 wahala

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,11 @@
-import ProductsContainer from '@/components/products/ProductsContainer';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import ProductsContainer from "@/components/products/ProductsContainer";
 
-async function ProductsPage({
-  searchParams,
-}: {
-  searchParams: { layout?: string; search?: string };
-}) {
-  const layout = searchParams?.layout ?? 'grid';
-  const search = searchParams?.search ?? '';
+
+
+async function ProductsPage({ searchParams }: any) {
+  const layout = searchParams?.layout ?? "grid";
+  const search = searchParams?.search ?? "";
   return (
     <>
       <ProductsContainer layout={layout} search={search} />
